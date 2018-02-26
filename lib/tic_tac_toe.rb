5a90dce98c04aba253be479c30f9519abc85dfaa
Maverick Tesini <mavericktesini@gmail.com>
2018-02-26 19:49:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,17 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
-
+  WIN_COMBINATIONS = [
+    [0,1,2], #1
+    [3,4,5], #2
+    [6,7,8], #3
+    [0,3,6], #4
+    [1,4,7], #5
+    [2,5,8], #6
+    [0,4,8], #7
+    [2,4,6] #8 possible win scenarios
+  ]
+######################################################
   def position_taken?(board, index)
     !(board[index].nil? || board[index] == " ")
   end
@@ -74,16 +84,6 @@ class TicTacToe
     end
   end
 
-  WIN_COMBINATIONS = [
-    [0,1,2], #1
-    [3,4,5], #2
-    [6,7,8], #3
-    [0,3,6], #4
-    [1,4,7], #5
-    [2,5,8], #6
-    [0,4,8], #7
-    [2,4,6] #8 possible win scenarios
-  ]
 
   def won?(board)
     # board[x] compares to win_combos , and if win_combos[z] all == "x", then return the combo
